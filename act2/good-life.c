3c5364ca417e7836399a700070e8200f4d59852f
Jesse  Burdick-Pless <jb4411@cs.rit.edu>
2020-09-10 00:10:03 -0400

    Added a GRID_SIZE constant. Fixed brackets and indentation.

--- act2/good-life.c
+++ act2/good-life.c
@@ -23,169 +23,155 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void header(void)
-{
-      printf("\n\t..Welcome to the Game of life..\n");
+#define GRID_SIZE 20
+
+void header(void) {
+	printf("\n\t..Welcome to the Game of life..\n");
 }
 
-void copyArray(char src[20][20], char dest[20][20]){
-    int row, col;
-    for(row = 0; row<20; row++) {
-	    for(col = 0; col<20; col++) {
-		    dest[row][col] = src[row][col];
-	    }
-    }
+void copy_array(char src[][GRID_SIZE], char dest[][GRID_SIZE]) {
+	int row, col;
+	for(row = 0; row < GRID_SIZE; row++) {
+		for(col = 0; col < GRID_SIZE; col++) {
+			dest[row][col] = src[row][col];
+		}
+	}
 }
 
-void survivalRule(char life[][20])
-	/// ^ fix 13: removed unused parameters 'x' and 'y'
-{
-      char newlife[20][20] = {{' '}};
-      int row, col;
-      copyArray(life, newlife);
-      int neighbors = 0;
-      for(row = 0; row<20; row++)
-      {
-         for(col = 0; col<20; col++)
-         {
-         //if(life[row][col] == '*')  /// fix 9: removed '&' and changed " to
-	    if(row > 0) {
-	       if(col > 0 && life[row - 1][col - 1] == '*')
-                  ++neighbors;
-               if(life[row - 1][col] == '*')
-                  ++neighbors;
-               if(col < 19 && life[row - 1][col + 1] == '*')
-                  ++neighbors;
-	    }
-               if(col > 0 && life[row][col - 1] == '*')
-                  ++neighbors;
-               if(col <19 && life[row][col + 1] == '*')
-                  ++neighbors;
-	    if(row < 19) {
-               if(col > 0 && life[row + 1][col - 1] == '*')
-                  ++neighbors;
-               if(life[row + 1][col] == '*')
-                  ++neighbors;   
-	       if(col < 19 && life[row + 1][col + 1] == '*')
-                  ++neighbors;
-	    }
-            if(neighbors == 3) {
-		    newlife[row][col] = '*'; /// fix 10: removed an '='
-	    } else if(neighbors == 2 && life[row][col] == '*') {
-		    newlife[row][col] = '*';
-	    } else {
-		    newlife[row][col] = ' ';
-	    }
-            neighbors = 0;
-	 }
-      } 
-      copyArray(newlife, life);
-      return;
+/// fix 13: removed unused parameters 'x' and 'y'
+void survival_rule(char life[][GRID_SIZE]) {
+	char newlife[GRID_SIZE][GRID_SIZE] = {{' '}};
+	int row, col;
+	copy_array(life, newlife);
+	int neighbors = 0;
+	for(row = 0; row < GRID_SIZE; row++) {
+		for(col = 0; col < GRID_SIZE; col++) {
+			if(row > 0) {
+				if(col > 0 && life[row - 1][col - 1] == '*')
+					++neighbors;
+				if(life[row - 1][col] == '*')
+					++neighbors;
+				if(col < GRID_SIZE - 1 && life[row - 1][col + 1] == '*')
+					++neighbors;
+			}
+			if(col > 0 && life[row][col - 1] == '*')
+				++neighbors;
+			if(col < GRID_SIZE - 1 && life[row][col + 1] == '*')
+				++neighbors;
+			if(row < GRID_SIZE) {
+				if(col > 0 && life[row + 1][col - 1] == '*')
+					++neighbors;
+				if(life[row + 1][col] == '*')
+					++neighbors;   
+				if(col < GRID_SIZE - 1 && life[row + 1][col + 1] == '*')
+					++neighbors;
+			}
+			if(neighbors == 3) {
+				newlife[row][col] = '*'; 
+				/// fix 10: removed an '='
+			} else if(neighbors == 2 && life[row][col] == '*') {
+				newlife[row][col] = '*';
+			} else {
+				newlife[row][col] = ' ';
+			}
+			neighbors = 0;
+		}
+	} 
+	copy_array(newlife, life);
+	return;
 }
 
-void birthRule(char life[][20]) 
-	/// ^ fix 12: removed unused parameters 'x' and 'y'
-{
-      int row, col;
-      int neighbors = 0;
-      for(row = 1; row<19; row++)
-      {
-         for(col = 1; col<19; col++)
-         {
-            if(life[row][col] == ' ') /// fix 8: removed '&' and changed " to '
-            {
-               if(life[row - 1][col - 1] == '*')
-                  neighbors++;
-               if(life[row - 1][col] == '*')
-                  neighbors++;
-               if(life[row - 1][col + 1] == '*')
-                  neighbors++;
-               if(life[row][col - 1] == '*')
-                  neighbors++;
-               if(life[row][col + 1] == '*')
-                  neighbors++;
-               if(life[row + 1][col - 1] == '*')
-                  neighbors++;
-               if(life[row + 1][col] == '*')
-                  neighbors++;
-               if(life[row + 1][col + 1] == '*')
-                  neighbors++;
-               if(neighbors == 3)
-               {
-                   life[row][col] = '*'; /// fix 7: removed an '='
-               }
-            }
-         }
-      }
-      return;
+/// ^ fix 12: removed unused parameters 'x' and 'y'
+void birth_rule(char life[][GRID_SIZE]) {
+	int row, col;
+	int neighbors = 0;
+	for(row = 1; row < GRID_SIZE - 1; row++) {
+		for(col = 1; col < GRID_SIZE - 1; col++) {
+			if(life[row][col] == ' ') { 
+				/// fix 8: removed '&' and changed " to '
+				if(life[row - 1][col - 1] == '*')
+					neighbors++;
+				if(life[row - 1][col] == '*')
+					neighbors++;
+				if(life[row - 1][col + 1] == '*')
+					neighbors++;
+				if(life[row][col - 1] == '*')
+					neighbors++;
+				if(life[row][col + 1] == '*')
+					neighbors++;
+				if(life[row + 1][col - 1] == '*')
+					neighbors++;
+				if(life[row + 1][col] == '*')
+					neighbors++;
+				if(life[row + 1][col + 1] == '*')
+					neighbors++;
+				if(neighbors == 3) {
+					life[row][col] = '*'; 
+					/// fix 7: removed an '='
+				}
+			}
+		}
+	}
+	return;
 }
 
 
-int main(void)
-{
-      char life[20][20] = {{' '}}; /// fix 2: initlized array
-      int orgs; /// fix 11: removed unused varible 'gens'
-      int i = 0, row = 0, col = 0; /// fix 2: initlized varibles
-      /// ^ fix 6: removed unused varibles 'a' and 'b'
-      int count = 0;
-      /// fix 17: removed unused varible 'int x'
-      /// fix 16: removed unused varible 'int y'
-      /// fix 5: removed unused varible 'char ans'
+int main(void) {
+	char life[GRID_SIZE][GRID_SIZE] = {{' '}}; /// fix 2: initlized array
+	int orgs; /// fix 11: removed unused varible 'gens'
+	int i = 0, row = 0, col = 0; /// fix 2: initlized varibles
+	/// ^ fix 6: removed unused varibles 'a' and 'b'
+	int count = 0;
+	/// fix 17: removed unused varible 'int x'
+	/// fix 16: removed unused varible 'int y'
+	/// fix 5: removed unused varible 'char ans'
 
-      header();
+	header();
 
-      printf("\nPlease enter the initial number of organisms: ");
-      scanf("%i", &orgs);
+	printf("\nPlease enter the initial number of organisms: ");
+	scanf("%i", &orgs);
+	puts(" ");
+	srand( 31 );
 
-      srand( 31 );
-      
-      while( i<orgs )
-      {
-          row = rand();
-          row %= 20;
-          col = rand();
-          col %= 20;
-	  if( life[row][col] != '*' ) {
-              life[row][col] = '*'; /// fix 4: removed an '='
-	      i++;
-	  } 
-      }
-      
-      for(row = 0; row<20; row++)
-      {
-          for(col = 0; col<20; col++)
-          {
-              if(life[row][col] != '*')
-                  life[row][col] = ' '; /// fix 3: removed an '='
-          }
-      }
+	while( i<orgs ) {
+		row = rand();
+		row %= GRID_SIZE;
+		col = rand();
+		col %= GRID_SIZE;
+		if( life[row][col] != '*' ) {
+			life[row][col] = '*'; /// fix 4: removed an '='
+			i++;
+		} 
+	}
 
-      for(row = 0; row<20; row++)
-      {
-          for(col = 0; col<20; col++)
-          {
-              printf("%c", life[row][col]); /// fix 1: changed %s to %c
-          }
-          puts(" ");
-      }
+	for(row = 0; row < GRID_SIZE; row++) {
+		for(col = 0; col < GRID_SIZE; col++) {
+			if(life[row][col] != '*')
+				life[row][col] = ' '; /// fix 3: removed an '='
+			printf("%c", life[row][col]); 
+			/// fix 1: changed %s to %c
+		}
+		puts(" "); 
+		/// added functions to print generation 0 to this loop
+	}
 
-      printf("\ngeneration: 0\n");
+	printf("\ngeneration: 0\n");
 
-      while ( count < 100 ) {
-          //birthRule(life); /// fix 14: removed excess arguments 'x' & 'y'
-          survivalRule(life); /// fix 15: removed excess arguments 'x' & 'y'
-          for(row = 0; row<20; row++)
-          {
-              for(col = 0; col<20; col++)
-              {
-                  printf("%c", life[row][col]); /// fix 1: changed %s to %c
-              }
-              puts(" ");
-          }
-          printf("\ngeneration: %d\n", count+1);
-          count++;
-      }
+	while ( count < 100 ) {
+		//birthRule(life); ///fix 14: removed excess arguments 'x' & 'y'
+		survival_rule(life); 
+		/// fix 15: removed excess arguments 'x' & 'y'
+		for(row = 0; row < GRID_SIZE; row++) {
+			for(col = 0; col < GRID_SIZE; col++) {
+				printf("%c", life[row][col]); 
+				/// fix 1: changed %s to %c
+			}
+			puts(" ");
+		}
+		printf("\ngeneration: %d\n", count+1);
+		count++;
+	}
 
-      return 0;
+	return 0;
 }
 
